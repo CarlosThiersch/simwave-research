24e09999a65c7303387bdb1251562639a9608d1e
Carlos Thiersch <104000871+CarlosThiersch@users.noreply.github.com>
2024-04-11 11:09:59 -0300

    Update gradient.cpp

--- simwave/kernel/backend/c_code/normal/gradient.cpp
+++ simwave/kernel/backend/c_code/normal/gradient.cpp
@@ -1273,13 +1273,13 @@ extern "C" double fast_gradient(f_type *v, f_type *grad, f_type *velocity, f_typ
             printf("Recovering checkpoint %d in timestep %ld\n", snapshots.top().index, n);
 
             u_snapshot = snapshot_d_current;                        
-
-        }else if(n% saving_stride == 0){
+            count = 0;
+        /*}else if(n% saving_stride == 0){
             
             printf("Recovering checkpoint %d in timestep %ld\n", snapshots.top().index, n);
 
             u_snapshot = snapshot_d_prev;
-            count = 0;     
+            count = 0;*/     
         }else if (count ==0){                       
             // recompute
             fast_forward_checkpoint(u, snapshot_d_prev, snapshot_d_current, velocity, damp,
@@ -1329,7 +1329,7 @@ extern "C" double fast_gradient(f_type *v, f_type *grad, f_type *velocity, f_typ
         }
 
         // remove the recent used snapshot
-        if( n % saving_stride == 0 ){
+        if( (n-1) % saving_stride == 0 ){
             // remove the the current snapshot from the stack
             free(snapshots.top().prev);
             free(snapshots.top().current);
